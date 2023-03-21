a7e3d8ff57f5c15f82772d85104f8bac471a017e
greatnesspersonified <akwangumo@gmail.com>
2023-03-21 18:13:38 +0100

    Formatting Dates

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-import datetime
-
-print(datetime.datetime.now())
-print(datetime.date.today())
-print(datetime.datetime.now().time())
-
 from datetime import datetime
 from datetime import date
 
-print(datetime.now())
-print(datetime.now().year)
-print(datetime.now().month)
-print(datetime.now().day)
-print(date.today())
-print(datetime.now().time())
+now = datetime.now()
+print(now)
+
+print(now.strftime("%d/%m/%Y %H:%M:%S"))
+print(now.strftime("%d/%B/%Y %H:%M:%S"))
+print(now.strftime("%d/%b/%Y %H:%M:%S"))
+print(date.today().strftime("%d-%m-%Y"))
+print(date.today().strftime("%d-%B-%Y"))
