7013df2c215b3d91230984fd90d62204092e7927
greatnesspersonified <akwangumo@gmail.com>
2023-03-23 10:41:49 +0100

    Creating files

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-from datetime import datetime
-from datetime import date
-
-now = datetime.now()
-print(now)
-
-print(now.strftime("%d/%m/%Y %H:%M:%S"))
-print(now.strftime("%d/%B/%Y %H:%M:%S"))
-print(now.strftime("%d/%b/%Y %H:%M:%S"))
-print(date.today().strftime("%d-%m-%Y"))
-print(date.today().strftime("%d-%B-%Y"))
+import os.path
+filename = "./data.csv"
+if os.path.isfile(filename):
+    with open("./data.csv", "r") as file:
+        print(file.read())
+else:
+    print(f"file {filename} does not exist")
